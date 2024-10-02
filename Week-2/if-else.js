d8633367758402e38891b54b70e09686c3e0552d
Sam_76 <muhammadusamah76@gmail.com>
2024-10-02 09:36:28 +0700

    JS Logic

---
+++ Week-2/if-else.js
@@ -0,0 +1,15 @@
+const reslut = 85;
+
+if (reslut > 90) {
+    console.log('Grade : A')
+} else if (reslut > 80){
+    console.log('Grade : B')
+} else if (reslut > 70){
+    console.log('Grade : C')
+} else {
+    console.log('Grade : D')
+}
+
+const age = 21;
+
+age > 21 ? console.log("Dewasa") : console.log("Belum Dewasa") 
