b548d2d4d639e51510ab7cee2fb21051d10ce5fe
Sam_76 <muhammadusamah76@gmail.com>
2024-10-02 08:51:30 +0700

    Tugas 1 finished

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
-gajiPokok = 0;
-const tunjangan = 0;
+var gajiPokok = 0;
+var tunjangan = 0;
 const data = {
     "golongan" : "A",
     "status" : "nikah"
@@ -11,9 +11,27 @@ if(golongan == "A"){
     gajiPokok = 10000000;
     console.log("Gaji Pokok = 10.000.000");
 } else if (golongan == "B"){
+    gajiPokok = 8000000;
     console.log("Gaji Pokok = 8.000.000");
 } else {
+    gajiPokok = 5000000;
     console.log("Gaji Pokok = 5.000.000");
 }
 
-console.log(gajiPokok);
+const statusTunjangan = data.status;
+
+if (statusTunjangan == "nikah") {
+    if(golongan == "A"){
+        tunjangan = 2000000;
+        console.log("Tunjangan = 2.000.000");
+    } else if (golongan == "B"){
+        tunjangan = 1500000;
+        console.log("Tunjangan  = 1.500.000");
+    } else {
+        tunjangan = 1000000;
+        console.log("Tunjangan = 1.000.000");
+    }
+};
+
+console.log(`Total : ${gajiPokok + tunjangan}`);
+
